119b346b3031bf1029605ef77dbb3f4228d92fb4
johnstechnics <severals.gm2@gmail.com>
2022-12-19 18:21:00 +0300

    route

--- src/api.js
+++ src/api.js
@@ -6,14 +6,14 @@ require('dotenv').config({path: `${__dirname}./../.env`});
 
 const BASE_URL = process.env.BASE_URL;
 const X_API_KEY = process.env.X_API_KEY;
-console.log(BASE_URL);
+// console.log(BASE_URL);
 
 router.route('/images/search')
 .get((req, res) => {
-    console.log(req.query.size);
+    // console.log(req.query.size);
     axios.get(`${BASE_URL}/images/search${req.query.size ? `?size=${req.query.size}` : ''}`)
     .then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         res.json(data.data);
     });
 });
@@ -22,11 +22,13 @@ router.route('/votes')
 .get((req, res) => {
     axios.get(`${BASE_URL}/votes`, { headers: { 'x-api-key': X_API_KEY } })
     .then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         res.json(data.data);
     });
 })
 .post((req, res) => {
+    console.log(req.body);
+    console.log(req.headers);
     axios.post(`${BASE_URL}/votes`, {
         'image_id': req.body.id,
         'value': req.body.voteValue
@@ -47,7 +49,7 @@ router.route('/favourites')
 .get((req, res) => {
     axios.get(`${BASE_URL}/favourites`, { headers: { 'x-api-key': X_API_KEY } })
     .then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         res.json(data.data);
     });
 });
@@ -56,7 +58,7 @@ router.route('/breeds')
 .get((req, res) => {
     axios.get(`${BASE_URL}/breeds`, { headers: { 'x-api-key': X_API_KEY } })
     .then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         res.json(data.data);
     });
 });
