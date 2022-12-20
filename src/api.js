f62b6e1005bf46bcd1c65a3db10817d3f86606d2
johnstechnics <severals.gm2@gmail.com>
2022-12-20 16:28:00 +0300

    url

--- src/api.js
+++ src/api.js
@@ -52,7 +52,7 @@ router.route('/images/:id')
 router.route('/images/upload')
 .post((req, res) => {
     console.log('req.files.file:', req.files.file);
-    axios.post(`${BASE_URL}/images/upload`, req.files.file, { headers: {
+    axios.post(`${BASE_URL}/images/upload`, JSON.stringify(req.files.file), { headers: {
         'x-api-key': X_API_KEY,
         'Content-Type': 'multipart/form-data'
     }})
