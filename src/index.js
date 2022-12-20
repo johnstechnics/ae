85ab85ecc4d4ccd5d8ffd4cd775ba91f8dc6261a
johnstechnics <severals.gm2@gmail.com>
2022-12-20 22:03:25 +0300

    r

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 // const fileUpload = require('express-fileupload');
+const app = express();
+const breeds = require('./routes/breeds');
 const votes = require('./routes/votes');
 const favourites = require('./routes/favourites');
 const images = require('./routes/images');
-const app = express();
 require('dotenv').config({path: `${__dirname}./../.env`});
 const PORT = process.env.PORT;
 
@@ -12,6 +13,7 @@ app.use(cors());
 // app.use(fileUpload());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api/breeds', breeds);
 app.use('/api/votes', votes);
 app.use('/api/favourites', favourites);
 app.use('/api/images', images);
---
+++ src/routes/breeds.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { default: axios } = require('axios');
+const { query } = require('express');
+require('dotenv').config({path: `${__dirname}./../../../.env`});
+
+const BASE_URL = process.env.BASE_URL;
+const X_API_KEY = process.env.X_API_KEY;
+
+router.route('/')
+.get((req, res) => {
+    axios.get(`${BASE_URL}/breeds`, { headers: { 'x-api-key': X_API_KEY } })
+    .then(data => {
+        res.json(data.data);
+    });
+});
+
+module.exports = router;
